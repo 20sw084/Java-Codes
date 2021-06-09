fa15ea20bfef81d6ec417092c263f404a346e483
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-06-09 22:09:11 +0500

    Add files via upload
    This Program is Taking input on java console method 1/3

---
+++ Taking_Input_on_Java_Console .java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Anoth {
+	
+	public static void main() {
+		
+		System.out.println("Enter a Number");
+		Scanner in=new Scanner(System.in);
+		int s= in.nextInt();
+		
+	}
+	
+}
