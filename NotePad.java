906ae3c0ce364a32e2e4f7696e8004e0285a686f
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-06-12 13:23:35 +0500

    Add files via upload
    String Use in Java.
    For Storing Name and Roll Number

---
+++ NotePad.java
@@ -0,0 +1,10 @@
+public class NotePad
+{
+	public static void main(String Args[])
+		{
+			String Name="Jonny Rajput";
+			String Roll_No="084";
+			System.out.println("Name: "+Name);
+			System.out.println("Roll Number: "+Roll_No);
+		}
+}
