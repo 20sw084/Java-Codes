c5ad6c655bd9858eddd5922abb9f59ce1abf6c9c
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-08-08 18:33:55 +0500

    Create 3.3.java
    A lottery requires that you select six different numbers from the integers 1 to 49.
     Write a program to do this for you and generate five sets of entries.

---
+++ 3.3.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String args[]) {
+    int num=50;
+    for(int i=1;i<6;i++){
+        int First=(int)(50*Math.random());
+        int Second=(int)(50*Math.random());
+        int Third=(int)(50*Math.random());
+        int Fourth=(int)(50*Math.random());
+        int Fifth=(int)(50*Math.random());
+      System.out.println(First+"\t"+Second+"\t"+Third+"\t"+Fourth+"\t"+Fifth);  
+    }
+  }
+}
