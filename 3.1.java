374aa0a3b8ca1ee0dd79ecf853d9089123be3592
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-08-08 17:45:57 +0500

    Create 3.1.java
    Write a program to display a random choice from a set of six choices for breakfast
     (you could use any set—for example, scrambled eggs, waffles, fruit, cereal, toast, or yogurt).

---
+++ 3.1.java
@@ -0,0 +1,26 @@
+public class Main{
+    public static void main(String[] args) {
+        int randomChoice=0;
+        randomChoice=(int) (6*Math.random());
+        switch(randomChoice){
+            case 1:
+            System.out.println("The breakfast is scrambled eggs");
+            break;
+            case 2:
+            System.out.println("The breakfast is waffles");
+            break;
+            case 3:
+            System.out.println("The breakfast is fruit");
+            break;
+            case 4:
+            System.out.println("The breakfast is cereal");
+            break;
+            case 5:
+            System.out.println("The breakfast is toast");
+            break;
+            case 6:
+            System.out.println("The breakfast is yougurt");
+            break;
+        }   
+    }
+}
