35b6f8b252b9a5d0c37f5fa7e43fa19a2535f5e8
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-08-18 12:20:04 +0500

    Create Difference btw static and local variable.java
    static variables can be accessed directly without
    	 * any instances. Just to demonstrate that static variables
    	 * are shared, I am accessing them using objects so that 
    	 * we can check that the changes made to static variables
    	 * by one object, reflects when we access them using other
    	 * objects

---
+++ Difference btw static and local variable.java
@@ -0,0 +1,10 @@
+  class Main{
+	static int num1;
+	String str1;
+	public static void main(String Args[]) {
+		num1=30;
+		Main M1=new Main();
+		M1.str1="Jonny";
+		System.out.println(num1+"\n"+M1.str1);
+	}
+}
