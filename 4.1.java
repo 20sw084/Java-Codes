c5a2dce579b7f319c175799d31d1af0fa07c2161
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-08-18 14:45:30 +0500

    Create 4.1.java
    Create an array of String variables and initialize the array with the names of the months from January to December.
     Create an array containing 12 random decimal values between 0.0 and 100.0.
     Display the names of each month along with the corresponding decimal value.
     Calculate and display the average of the 12 decimal values.

---
+++ 4.1.java
@@ -0,0 +1,16 @@
+class Main{
+	public static void Cout(String a)
+	{
+		System.out.println(a);
+	}
+	public static void main(String A[]) {
+		double average=0.0;
+		String[] strArray={"January","Februry","March","April","May","June","July","August","September","October","November","December"};
+		for(int i=0;i<12;i++) {
+		double random=12*Math.random();
+		Cout(strArray[(int) random]+" is at "+random);
+		average+=random;
+		}
+		Cout("Average is "+average);
+	}
+}
