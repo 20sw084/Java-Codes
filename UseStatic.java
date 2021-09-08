f60b0e06305611da0a035c935672be9454538ad9
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-09-08 19:59:47 +0500

    Add files via upload
    Task Assigned by Mam Mariyam (Practical 05).

---
+++ UseStatic.java
@@ -0,0 +1,12 @@
+public class UseStatic {
+    
+    public static void main(String[] args) {
+        print("Jonny Rajput");              //We Don`t need to create an 
+                                            //OBJECT. Static methods can 
+                                            //easily be called without 
+                                            //making an object. 
+    }
+    public static void print(String a) {
+        System.out.println(a);
+    }
+}
