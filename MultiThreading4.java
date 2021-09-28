079a355dd61e5cac255b55d298ab34eaf433fa7e
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-09-28 20:21:17 +0500

    Create MultiThreading4.java
    getpriority method and show method with interruptedException throws as there is sleep method of thread class.

---
+++ MultiThreading4.java
@@ -0,0 +1,31 @@
+//MultiThreading.java
+
+public class MultiThreading implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Run Block is Running of ID "+Thread.currentThread().getId());
+        System.out.println("Name of Thread is "+Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getPriority());
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ie){
+            ie.getStackTrace();
+        }
+        System.out.println("Run Block is Ends here. "+Thread.currentThread().getName());
+    }
+    public void show() throws InterruptedException{
+        Thread.sleep(-2122);
+    }
+}
+
+//Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        Thread MT1=new Thread(new MultiThreading(),"MT1");
+        MT1.start();  //Used when every thread is executed
+        Thread MT2=new Thread(new MultiThreading(),"MT2");
+        MT2.start();
+   }
+}
