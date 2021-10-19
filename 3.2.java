1f2af5cfe73f01c3c41c247aff520130c0396b66
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-10-19 21:51:22 +0500

    Update 3.2.java

--- 3.2.java
+++ 3.2.java
@@ -3,7 +3,7 @@ public class Main{
     int maxValue = 50;                                                 // The maximum value to be checked.
 
     // Check all values from 2 to maxValue:
-    for(int i = 2 ; i <= maxValue ; ++i) {
+    for(int i = 2 ; i <= maxValue ; ++i) {   
       // Try dividing by all integers from 2 to square root of i:
       for(int j = 2 ; j <= Math.sqrt(i) ; ++j) {
         if(i%j == 0)                                                   // This is true if j divides exactly
